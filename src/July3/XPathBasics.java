76980e60733e36a7c1e189cd0fc4fd42a406b949
Duotech Academy <dtacademyb5@gmail.com>
2021-07-03 15:19:07 -0400

    July2 fourth commit

---
+++ src/July3/XPathBasics.java
@@ -0,0 +1,64 @@
+package July3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class XPathBasics {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nuclues\\Documents\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver(); // launches a new browser session
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
+
+
+        driver.get("https://www.duotech.io/");
+
+
+        List<WebElement> div = driver.findElements(By.xpath("//div")); //retrives all a elements from the page
+
+        System.out.println(div.size());
+
+        String fullXpath = "/html/body/div[2]/div[3]/div/h1";
+
+        System.out.println(driver.findElement(By.xpath(fullXpath)).getText());
+
+        String relativeXpath = "//h1";
+
+        System.out.println(driver.findElement(By.xpath(relativeXpath)).getText());
+
+
+        String commonXpathWithAttributes = "//p[@class='paragraph-big']";
+
+        //p[@class="paragraph-big"]  you can also use double quotes
+
+        System.out.println(driver.findElement(By.xpath("//p[@class='paragraph-big']")).getText()); // single attribute
+
+
+        System.out.println(driver.findElement(By.xpath("//a[@class='link-2'][@href='https://duotech.talentlms.com/']")).getText());
+        // multuple attributes
+
+        System.out.println(driver.findElement(By.xpath("//a[.='Contacts']")).getText());
+
+        String containsText = "//p[   contains(    text()  , 'Business Systems Analyst and'    )]";
+
+        System.out.println(driver.findElement(By.xpath(containsText)).getText());
+
+
+
+
+
+
+
+    }
+}
